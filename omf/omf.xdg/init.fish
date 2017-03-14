262ad255555aee3f1c1a40747c848ce68f0d44cb
Henrik W Lund <henrik.w.lund@gmail.com>
2017-03-14 11:20:27 +0100

    Fixed path for gnupg@2.1 after homebrew moved it

--- omf/omf.xdg/init.fish
+++ omf/omf.xdg/init.fish
@@ -3,5 +3,6 @@ function fish_greeting
 end
 
 #GPG config stuff
+set -g fish_user_paths "/usr/local/opt/gnupg@2.1/bin" $fish_user_paths
 set -xg GPG_TTY (tty)
 set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
