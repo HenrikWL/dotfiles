1f1f5da570a768eec40afeca0af169c49b0ced4b
Henrik W Lund <henrik.w.lund@gmail.com>
2017-03-21 13:18:06 +0100

    Added support for machine-local fish config

---
+++ omf/omf.xdg/README.md
@@ -0,0 +1,2 @@
+# Machine-local config for fish
+Put any machine-local config for fish into a file named init-local.fish in this folder. It will not be checked into git, but will be picked up by init.fish
--- omf/omf.xdg/init.fish
+++ omf/omf.xdg/init.fish
@@ -6,3 +6,7 @@ end
 set -g fish_user_paths "/usr/local/opt/gnupg@2.1/bin" $fish_user_paths
 set -xg GPG_TTY (tty)
 set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
+#Source local config if it exists
+test -f init-local.fish
+  and source init-local.fish
