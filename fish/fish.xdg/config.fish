a500f51b1b44ee30c18c672ac08541138d413623
Henrik W Lund <henrik.w.lund@gmail.com>
2019-01-23 08:47:41 +0100

    Removed fisherman from version control

--- fish/fish.xdg/config.fish
+++ fish/fish.xdg/config.fish
@@ -9,3 +9,9 @@ which psql > /dev/null
 test -f /Library/PostgreSQL/9.3/pg_env.sh -a $status -ne 0
 	and bass source /Library/PostgreSQL/9.3/pg_env.sh
 
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
